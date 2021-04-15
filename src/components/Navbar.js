3484a9016dcc175d27149ba212d3564648b1ec78
Adrian Duran <duranadria99@gmail.com>
2021-04-15 23:19:36 +0200

    Optimizacion de la landing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ class Navbar extends LitElement {
     return html`
       <header class="header-main">
         <a href="./index.html">
-          <img class="logo" src="/logo.png" alt="YekaCoin logo">
+          <img class="logo" src="/logo-yekacoin.webp" alt="YekaCoin logo">
         </a>
         <slot></slot>
       </header>
